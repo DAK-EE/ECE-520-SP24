72429e20af61c209c7733dc53764f53281dbfcff
DAK-EE <kingdie@sonoma.edu>
2024-03-26 20:01:44 -0700

    updated and renamed SSEG project to SSEGKeypad

--- SSEGKeypad.X/SSEGmain.asm
+++ SSEGKeypad.X/SSEGmain.asm
@@ -1,85 +1,111 @@
 
+/*
+
+    Title:	    7-Segment LED Keypad
+    Author:	    Diego King
+    Date:	    March 26, 2024
+    IDE:	    MPLABX v6.20
+    Device:	    PIC18F47K42 CURIOSITY NANO
+    Version:	    2
+    Description:   This program reads a 4x4 matrix keypad (only the first
+row and three columns are used) and drives a 7-Segment LED (active low) based on the key
+which is pressed:
+		1 => Increment (roll-over if F)
+		2 => Decrement (roll-under if 0)
+		3 => Reset
+    Inputs:	    RB[3:7]
+    Outputs:	    RB[0:2], RD[0:7]
+    
+    */
 ;INCLUDES, HEADERS, PSECT SCRIPTS
-#include "D:\Documents\ECE 520\AssemblyConfig.inc"
-#include <xc.inc>
+#include "AssemblyConfig.inc"
 PSECT absdata,abs,ovrld
 ;DEFINES AND VARIABLES
-    DINDEXLO EQU 0x20
-    DINDEXHI EQU 0x21
+    DINDEXLO EQU 0x20 ;used for _delay Subroutine
+    DINDEXHI EQU 0x21 ;used for _delay Subroutine
 
 ;INITIALIZATION
     CALL _setupPortD
     CALL _setupPortB
-    CALL _zero ;INITIALIZE TBLPTR TO 0x000101
-    ORG 0x20
+    CALL _ssegZero ;INITIALIZE TBLPTR TO 0x000D1
 ;PROGRAM START
 _main:
     CALL _readKeys
-    TSTFSZ WREG, 0
+    TSTFSZ WREG, 0 ;WREG is 0 when no key is pressed
     CALL _keyFunctions
     GOTO _main
 _readKeys:
 ;   TEST COLUMN 1
     CLRF WREG
     BSF PORTB, 0
-    NOP
     BTFSC PORTB, 3
     MOVLW 1	
     BCF PORTB, 0
     
 ;   TEST COLUMN 2
     BSF PORTB, 1
-    NOP
     BTFSC PORTB, 3
     MOVLW 2
     BCF PORTB, 1
     
 ;   TEST COLUMN 3
     BSF PORTB, 2
-    NOP
     BTFSC PORTB, 3
     MOVLW 3	
     BCF PORTB, 2
     RETURN
 _keyFunctions:
+    ;This subroutine takes the output of _readKeys (WREG)
+    ;Case WREG
+    ;	1 -> 2-1=1 therefore N and Z flags not triggered, move to _increment
+    ;	2 -> 2-2=0 therefore Z flag triggered, move to _decrement
+    ;	3 -> 2-3=-1 therefore N flag triggered, move to _zero
     SUBLW 2
-    BN _zero
+    BN _ssegZero
     BZ _decrement
     BRA _increment
-_zero:
+_ssegZero:
+;    ORG 0x000D0
     MOVLW 0x00
     MOVWF TBLPTRU, 0
-    MOVLW 0x01
+    MOVLW 0x00
     MOVWF TBLPTRH, 0
-    MOVLW 0x01
+    MOVLW 0xD1
     MOVWF TBLPTRL, 0
     TBLRD*
-    MOVFF TABLAT, PORTD
-    RETURN
+    MOVFF TABLAT, LATD
+    GOTO _delay
+_ssegF:
+    MOVLW 0x00
+    MOVWF TBLPTRU, 0
+    MOVLW 0x00
+    MOVWF TBLPTRH, 0
+    MOVLW 0xE0
+    MOVWF TBLPTRL, 0
+    TBLRD*
+    MOVFF TABLAT, LATD
+    GOTO _delay
 _decrement:
     DECF TBLPTR, 1, 0
     TBLRD*
     BTFSS TABLAT, 7, 0
-    GOTO _increment
-    MOVFF TABLAT, PORTD
-    CALL _delay
-    RETURN
+    GOTO _ssegF
+    MOVFF TABLAT, LATD
+    GOTO _delay
 _increment:
     TBLRD+*
     BTFSS TABLAT, 7, 0
-    GOTO _decrement
-    MOVFF TABLAT, PORTD
-    CALL _delay
-    RETURN
+    GOTO _ssegZero
+    MOVFF TABLAT, LATD
+    GOTO _delay
 _delay:			;Subroutine takes around 130k instruction cycles
     CLRF DINDEXLO, 0
     CLRF DINDEXHI, 0
-    BSF STATUS,0, 0
 _loop:
     INCFSZ DINDEXLO, 1, 0
     GOTO _loop
     INCF DINDEXHI, 1, 0
-    BTFSS DINDEXHI, 5, 0
+    TSTFSZ DINDEXHI, 0
     GOTO _loop
     RETURN
 ;SETTING UP PORTD
@@ -106,11 +132,9 @@ _setupPortB:
     MOVLW	0b11111000 ;outputs RB[0:2], inputs RB[3:7]
     MOVWF	TRISB ;
     RETURN
-    ORG 0x100
+    ORG 0x000D0
 ;    SSEG DECODE LUT FOR:
 ;    1. ACTIVE LOW (COMMON ANODE)
 ;    2. DP IS MSB
-;    [a b c d e f g rdp] = [0 1 2 3 4 5 6 7]
-;    DB 0x00, 0x03, 0x9F, 0x25, 0x0D, 0x99, 0x49, 0x41, 0x1F, 0x01, 0x19, 0x00
 ;    [rdp g f e d c b a] = [7 6 5 4 3 2 1 0]
-    DB 0x00, 0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x98, 0x00
+    DB 0x00, 0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x98, 0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E, 0x00
